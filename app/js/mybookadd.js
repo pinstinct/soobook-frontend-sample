42bdd7517806d4883371a5a609a27d176231bc0f
LimHM <gusals3407@gmail.com>
2017-04-21 08:58:37 +0900

    mybook add API 호출

---
+++ app/js/mybookadd.js
@@ -0,0 +1,20 @@
+function mybookaddjs(book) {
+  var token = 'Token ' + getCookie('SoobookToken');
+  $.ajax({
+    url: 'https://soobook.devlim.net/api/book/mybook/',
+    type: 'POST',
+    headers: {
+      Authorization: token,
+    },
+    dataType: 'json',
+    data: {
+      book_id: book,
+    }
+  })
+  .done(function(response) {
+    console.log(response);
+  })
+  .fail(function(response) {
+    console.log(response)
+  });
+}
